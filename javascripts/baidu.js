48cf7d1fdf3cf388049d85d333690f872519820a
zhanwang <jian_wang_zhan@163.com>
2013-07-31 10:15:45 +0800

    start again

--- javascripts/baidu.js
+++
@@ -1,4 +0,0 @@
-
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fccb208b8a5746a3dd6f133ca1e67be7f' type='text/javascript'%3E%3C/script%3E"));
-
